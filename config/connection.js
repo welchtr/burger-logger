01ea58411e9b43e0ec8151963ce571d33460acd8
Tiara Welch <tiarawelch@Tiaras-MacBook-Pro.local>
2018-03-10 10:43:06 -0500

    making small progress. Still missing major components

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "cat_db"
+  database: "burger_db"
 });
 
 // Make connection.
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO pizza (pizza_name) VALUES ('hamburger');
+INSERT INTO pizza (pizza_name) VALUES ('veggie burger');
+INSERT INTO pizza (pizza_name) VALUES ('cheeseburger');
